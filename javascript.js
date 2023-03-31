7b8ba0cc91692f98965cb8c926913f485b126f2f
AnaSanchez95 <120680319+AnaSanchez95@users.noreply.github.com>
2023-03-31 21:28:57 +0200

    Create html and javascript

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice () {
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissors = "Scirssors";
+    
+    return 
+}
+
+function gameRound (playerSelection, computerSelection) {
+
+}
